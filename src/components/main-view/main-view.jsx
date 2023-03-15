073d4e4104e7613ea244a3a2a1bb914683bd263d
Katie Sharples <KSharples431@gmail.com>
2023-03-15 16:31:33 -0400

    Fixed conflict

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BookCard } from '../book-card/book-card';
 import { BookView } from '../book-view/book-view';
 import { LoginView } from '../login-view/login-view';
@@ -11,56 +11,32 @@ export const MainView = () => {
   const [user, setUser] = useState(storedUser ? storedUser : null);
   const [token, setToken] = useState(storedToken ? storedToken : null);
   const [books, setBooks] = useState([]);
-  const [books, setBooks] = useState([
-    {
-      id: 1,
-      number: 'Book 6',
-      title: 'Code Zero',
-      author: 'Jonathan Mayberry',
-      genre: 'Science Fiction',
-      series: 'Joe Ledger',
-      image:
-        'https://m.media-amazon.com/images/I/51SOvsjctML._SY344_BO1,204,203,200_.jpg',
-    },
-    {
-      id: 2,
-      number: 'Book 7',
-      title: 'Predator One',
-      author: 'Jonathan Mayberry',
-      genre: 'Science Fiction',
-      series: 'Joe Ledger',
-      image: 'https://m.media-amazon.com/images/I/51HXmNQyfDL._SY300_.jpg',
-    },
-    {
-      id: 3,
-      number: 'Book 9',
-      title: 'Dogs of War',
-      author: 'Jonathan Mayberry',
-      genre: 'Science Fiction',
-      series: 'Joe Ledger',
-      image: 'https://m.media-amazon.com/images/I/61SewB7GEZL._SY300_.jpg',
-    },
-    {
-      id: 4,
-      number: 'Book 1',
-      title: 'Patient Zero',
-      author: 'Jonathan Mayberry',
-      genre: 'Science Fiction',
-      series: 'Joe Ledger',
-      image: 'https://m.media-amazon.com/images/I/41hEdTkxJ3L._SY300_.jpg',
-    },
-    {
-      id: 5,
-      number: 'Book 5',
-      title: 'Extinction Machine',
-      author: 'Jonathan Mayberry',
-      genre: 'Science Fiction',
-      series: 'Joe Ledger',
-      image: 'https://m.media-amazon.com/images/I/51lIV+TNq2L._SY300_.jpg',
-    },
-  ]);
-
   const [selectedBook, setSelectedBook] = useState(null);
+
+  useEffect(() => {
+    fetch('https://my-books-series-tracker.herokuapp.com/books')
+      .then((response) => response.json())
+      .then((data) => {
+        const booksFromApi = data.map((book) => {
+          return {
+            id: book._id,
+            image: book.image,
+            title: book.title,
+            author: book.author,
+            genre: book.genre,
+            series: book.series,
+            number: book.number,
+            description: book.description,
+            owned: book.owned,
+            // availibility: book.availibility,
+            read: book.read,
+            favorite: book.favorite,
+          };
+        });
+
+        setBooks(booksFromApi);
+      });
+  }, []);
 
   if (selectedBook) {
     return (
